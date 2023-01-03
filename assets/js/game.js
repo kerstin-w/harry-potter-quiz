15acad4b1510563d63e41cc400c6e698fce730ab
Kerstin <kerstinweibeler@gmail.com>
2023-01-03 16:14:56 +0000

    Add function to compare correct questions and increment User Score

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,14 +2,14 @@
 // Declare const for DOM elements
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const housePick = localStorage.getItem("housePick");
 const progressText = document.getElementById('progress-text');
 const scoreText = document.getElementById('score');
+const housePick = localStorage.getItem("housePick");
 const progressBar = document.getElementById('progress-bar');
 
 // Const with the value of points per correct question and max number of questions per game
-const correctQuestion = 10;
-const maxQuestions = 10;
+const pointsCorrectAnswer = 10;
+const maxQuestions= 10;
 
 //Declare variables for game
 let currentQuestion = {};
@@ -17,7 +17,8 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 let gameQuestions = [];
-let time = 2100;
+let time = 1500;
+let acceptingAnswers;
 
 //Wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
@@ -35,16 +36,22 @@ function newGame(housePick) {
     getNewQuestion();
     localStorage.setItem('housePick', housePick);
     console.log(housePick);
-    //localStorage.setItem('housePick', JSON.stringify(housePick))
     document.getElementById('house-logo').innerHTML = `<img src="assets/images/${housePick}.png" alt="Hogwards House Logo">`;
 }
 
+
 /**
- * Load new question
+ * Get a new question
  * */
 function getNewQuestion() {
-    
+    localStorage.setItem('lastScore', score);
+    if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+        return window.location.assign('game-end.html');;
+    }
+
     questionCounter++;
+    progressText.innerText = 'Question ' + questionCounter + '/' + maxQuestions;
+
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
@@ -53,15 +60,83 @@ function getNewQuestion() {
     choices.forEach(choice => {
         const choiceNumber = choice.dataset.choice;
         const image = currentQuestion['choice' + choiceNumber]
-        console.log(choice.firstElementChild);
 
         if (currentQuestion['choice' + choiceNumber].includes('assets/images')) {
             choice.replaceChildren();
             choice.innerHTML += `<img src=${image} data-choice="${choiceNumber}">`
         } else {
-            choice.firstElementChild.innerText = currentQuestion['choice' + choiceNumber];
+            choice.innerText = currentQuestion['choice' + choiceNumber];
         }
     });
 
     availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+    progressBar.value += 1;
+}
+
+
+/**
+ * Compare user's answer with correct answer. If it is true the user will get displayed a message in green and if it is wrong a message appears in red
+ * */
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset.choice;
+        const currentCorrectAnswer = currentQuestion.correctAnswer;
+        const currentCorrectAnswerBox = document.querySelector(`[data-choice="${currentCorrectAnswer}"]`);
+        console.log(selectedAnswer);
+        const classToApply =
+            selectedAnswer == currentQuestion.correctAnswer ? 'correct' : 'incorrect';
+
+        let correctAnswerReply = ['Correct'];
+        const correctAnswerReplyIndex = Math.floor(Math.random() * correctAnswerReply.length);
+        const corrrectAnswerReplyRandom = correctAnswerReply[correctAnswerReplyIndex];
+
+        let wrongAnswerReply = ['Wrong'];
+
+        const wrongAnswerReplyIndex = Math.floor(Math.random() * wrongAnswerReply.length);
+        const wrongAnswerReplyRandom = wrongAnswerReply[wrongAnswerReplyIndex];
+        selectedChoice.classList.add(classToApply);
+
+        if (selectedAnswer == currentQuestion.correctAnswer) {
+            incrementScore(pointsCorrectAnswer);
+
+            if (questionCounter <= 2) {
+                question.innerHTML = correctAnswerReply[1];
+            } else if (questionCounter == maxQuestions) {
+                question.innerHTML = correctAnswerReply[0];
+            } else {
+                question.innerHTML = corrrectAnswerReplyRandom;
+            }
+            question.classList.add('correct-text');
+
+        } else {
+            if (questionCounter == maxQuestions || questionCounter <= 2) {
+                question.innerHTML = wrongAnswerReply[0];
+            } else {
+                question.innerHTML = wrongAnswerReplyRandom;
+            }
+
+            question.classList.add('incorrect-text');
+            currentCorrectAnswerBox.classList.add('correct');
+
+        }
+
+        loadNewQuestion(() => {
+            selectedChoice.classList.remove(classToApply);
+            currentCorrectAnswerBox.classList.remove('correct');
+            question.classList.remove('correct-text');
+            question.classList.remove('incorrect-text');
+            getNewQuestion();
+        }, time);
+    });
+});
+
+function incrementScore(num) {
+    score += num;
+    scoreText.innerText = score;
 }
