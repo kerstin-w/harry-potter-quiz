fe1085414882ef67d0e7d5e3ed5889ac1535c247
Kerstin <kerstinweibeler@gmail.com>
2023-01-04 15:23:59 +0000

    Add map methode to show high scores on game-score page

---
+++ assets/js/game-score.js
@@ -0,0 +1,17 @@
+'use-strict';
+
+// Declare const for DOM elements
+const housePick = localStorage.getItem('housePick');;
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+/**
+ * Show High Score List
+ * Return name, score and which house was played
+ */
+highScoresList.innerHTML = highScores
+
+.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score} Points <img class="house small flex-center" src="assets/images/${score.house}.png" alt="House Log">`
+})
+.join("");
