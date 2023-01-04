290220c07f48491010bf0ebd251b381015265cbb
Kerstin <kerstinweibeler@gmail.com>
2023-01-04 14:55:22 +0000

    Add statement to show end score and function to save score + username

--- assets/js/game-end.js
+++ assets/js/game-end.js
@@ -2,10 +2,43 @@
 
 // Declare const for DOM elements
 const username = document.getElementById('username');
-const finalScore = document.getElementById('final-score');
+const endScore = document.getElementById('end-score');
 const lastScore = localStorage.getItem('lastScore');
 const housePick = localStorage.getItem('housePick');;
 const saveScore = document.getElementById('save-score');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 //Show users final game score
-finalScore.innerText = lastScore + " Points";
+if (lastScore >= 80) {
+    endScore.innerText = `${lastScore} Points! 
+    You are a true ${housePick}`;
+} else {
+    endScore.innerText = `${lastScore} Points!`;
+}
+
+/**
+ * Enable the save button
+ * */
+username.addEventListener('keyup', () => {
+    saveScore.disabled = !username.value;
+});
+
+/**
+ * Save username, score and which house was picked.
+ * Forward user to score page.
+ */
+saveHighScore = (e) => {
+    e.preventDefault();
+
+    const score = {
+        score: lastScore,
+        name: username.value,
+        house: housePick,
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(6);
+
+    window.location.assign('game-score.html');
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+};
