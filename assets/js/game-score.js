18d0c3256a760bead6cb80c73bde7f8bdd90d7aa
Kerstin <kerstinweibeler@gmail.com>
2023-01-11 11:02:16 +0000

    Adjust formating of high score table

--- assets/js/game-score.js
+++ assets/js/game-score.js
@@ -6,17 +6,15 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
  * Show High Score List
  * Return name, score and which house was played
  */
-highScoresList.innerHTML = highScores
-
-    .map((score) => {
-        return `
-    <tr>
-    <td>${score.name}</td>
-    <td>${score.score}</td>
-    <td><img class="small" src="assets/images/${score.house}.png" alt="House Logo"></td>
-    </tr>`;
-    })
-    .join("");
+highScoresList.innerHTML = highScores.map((score) => {
+    return `
+        <tr>
+            <td>${score.name}</td>
+            <td>${score.score}</td>
+            <td><img class="small" src="assets/images/${score.house}.png" alt="House Logo"></td>
+        </tr>
+    `;
+}).join("");
 
 function deleteStorage() {
     localStorage.clear();
